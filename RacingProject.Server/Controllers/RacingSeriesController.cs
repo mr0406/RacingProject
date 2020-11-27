5a163574e91169f7b2e13439df9fbbd2e436697e
mr0406 <64924553+mr0406@users.noreply.github.com>
2020-11-27 21:05:58 +0100

    Add RacingSeriesController

---
+++ RacingProject.Server/Controllers/RacingSeriesController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RacingProject.Server.Data;
+using RacingProject.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RacingProject.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RacingSeriesController : ControllerBase
+    {
+        private readonly RacingProjectContext _context;
+
+        public RacingSeriesController(RacingProjectContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<RacingSerie>> Get()
+        {
+            return _context.RacingSeries;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<RacingSerie> GetById(int id)
+        {
+            var racingSerie = _context.RacingSeries.Find(id);
+
+            if(racingSerie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(racingSerie);
+        }
+
+        [HttpPost]
+        public ActionResult<RacingSerie> Create(RacingSerie racingSerie)
+        {
+            _context.RacingSeries.Add(racingSerie);
+            _context.SaveChanges();
+
+            return Ok(racingSerie);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<RacingSerie> Update(int id, RacingSerie newRacingSerie)
+        {
+            var oldRacingSerie = _context.RacingSeries.Find(id);
+
+            if(oldRacingSerie == null)
+            {
+                return NotFound();
+            }
+
+            oldRacingSerie.Name = newRacingSerie.Name;
+            oldRacingSerie.Races = newRacingSerie.Races;
+
+            _context.SaveChanges();
+
+            return Ok(newRacingSerie);
+        }
+    }
+}
