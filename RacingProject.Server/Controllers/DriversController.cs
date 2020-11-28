24abe074051a5d6d4b03f782bc15f1e7eaba722a
mr0406 <64924553+mr0406@users.noreply.github.com>
2020-11-28 11:24:38 +0100

    Add RaceResultsController

--- RacingProject.Server/Controllers/DriversController.cs
+++ RacingProject.Server/Controllers/DriversController.cs
@@ -81,6 +81,5 @@ namespace RacingProject.Server.Controllers
 
             return Ok();
         }
-
     }
 }
---
+++ RacingProject.Server/Controllers/RaceResultsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RacingProject.Server.Data;
+using RacingProject.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RacingProject.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RaceResultsController : ControllerBase
+    {
+        private readonly RacingProjectContext _db;
+
+        public RaceResultsController(RacingProjectContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<RaceResult>> GetAll()
+        {
+            return _db.RaceResults;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<RaceResult> GetById(int id)
+        {
+            var raceResult = _db.RaceResults.Find(id);
+
+            if (raceResult == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(raceResult);
+        }
+
+        [HttpPost]
+        public ActionResult<RaceResult> Create(RaceResult raceResult)
+        {
+            _db.RaceResults.Add(raceResult);
+            _db.SaveChanges();
+
+            return Ok(raceResult);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<RaceResult> Update(int id, RaceResult newRaceResult)
+        {
+            var oldRaceResult = _db.RaceResults.Find(id);
+
+            if (oldRaceResult == null)
+            {
+                return NotFound();
+            }
+
+            oldRaceResult.DriverId = newRaceResult.DriverId;
+            oldRaceResult.RaceId = newRaceResult.RaceId;
+            oldRaceResult.StartingPosition = newRaceResult.StartingPosition;
+            oldRaceResult.FinalPosition = newRaceResult.FinalPosition;
+            oldRaceResult.ScoredPoints = newRaceResult.ScoredPoints;
+
+            _db.SaveChanges();
+
+            return Ok(oldRaceResult);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var raceResult = _db.RaceResults.Find(id);
+
+            if (raceResult == null)
+            {
+                return NotFound();
+            }
+
+            _db.RaceResults.Remove(raceResult);
+            _db.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
