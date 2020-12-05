e7c77f8eb35ed5950c3d810bd2bfa370134ea10a
mr0406 <64924553+mr0406@users.noreply.github.com>
2020-12-05 23:00:33 +0100

    JSON and XML Serialization done

---
+++ RacingProject.Server/Controllers/JSONController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RacingProject.Server.Data;
+using RacingProject.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RacingProject.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class JSONController : ControllerBase
+    {
+        private readonly RacingProjectContext _db;
+
+        public JSONController(RacingProjectContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Index()
+        {
+            string text = "Here you can use JSON serialization/deserialization \n" +
+                            "To read drivers: JSON/Drivers - GET \n" +
+                            "To write driver JSON/Drivers - POST \n";
+            return text;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Driver>> Drivers()
+        {
+            string filePath = Environment.CurrentDirectory + "\\JSON\\drivers.json";
+
+            List<Driver> drivers = new List<Driver>();
+            using(StreamReader reader = new StreamReader(filePath))
+            {
+                string readText = reader.ReadToEnd();
+                Debug.WriteLine(readText);
+                drivers = JsonConvert.DeserializeObject<List<Driver>>(readText);
+            }
+
+            return drivers;      
+        }
+
+        [HttpPost]
+        public ActionResult<List<Driver>> Drivers(Driver newDriver)
+        {
+            string filePath = Environment.CurrentDirectory + "\\JSON\\drivers.json";
+
+            List<Driver> drivers = new List<Driver>();
+            /*using (StreamReader reader = new StreamReader(filePath))
+            {
+                string readText = reader.ReadToEnd();
+                Debug.WriteLine(readText);
+                if(readText != null)
+                {
+                    drivers = JsonConvert.DeserializeObject<List<Driver>>(readText);
+                }
+            }*/
+
+            drivers.Add(newDriver);
+
+            string jsonData = JsonConvert.SerializeObject(drivers, Formatting.Indented);
+
+            using(StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.Write(jsonData);
+            }
+
+            return drivers;
+        }
+    }
+}
---
+++ RacingProject.Server/Controllers/XMLController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RacingProject.Server.Data;
+using RacingProject.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace RacingProject.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class XMLController : ControllerBase
+    {
+        
+
+        private readonly RacingProjectContext _db;
+        private readonly XmlSerializer xmlSerializer;
+        private readonly string _filePath;
+
+        public XMLController(RacingProjectContext db)
+        {
+            _db = db;
+            xmlSerializer = new XmlSerializer(typeof(List<Driver>));
+            _filePath = Environment.CurrentDirectory + "\\XML\\drivers.xml";
+        }
+
+        [HttpGet]
+        public ActionResult<string> Index()
+        {
+            string text = "Here you can use XML serialization/deserialization \n" +
+                            "To read drivers: XML/Drivers - GET \n" +
+                            "To write driver XML/Drivers - POST \n";
+            return text;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Driver>> Drivers()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(List<Driver>));
+
+            List<Driver> drivers = new List<Driver>();
+            using (StreamReader stream = new StreamReader(_filePath))
+            {
+                drivers = (List<Driver>)xmlSerializer.Deserialize(stream);
+            }
+
+            return drivers;
+        }
+
+        [HttpPost]
+        public ActionResult<List<Driver>> Drivers(Driver newDriver)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(List<Driver>));
+
+            List<Driver> drivers = new List<Driver>();
+            using (StreamReader stream = new StreamReader(_filePath))
+            {
+                drivers = (List<Driver>)xmlSerializer.Deserialize(stream);
+            }
+
+            drivers.Add(newDriver);
+
+            using (TextWriter textWriter = new StreamWriter(_filePath))
+            {
+                xmlSerializer.Serialize(textWriter, drivers);
+            }
+
+            return drivers;
+        }
+    }
+}
