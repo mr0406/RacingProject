bc22750c609222286cb64cf92e26d48dd0a96e6f
mr0406 <64924553+mr0406@users.noreply.github.com>
2020-12-11 17:37:18 +0100

    Create teams query

---
+++ RacingProject.Server/Controllers/ChartsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RacingProject.Server.Data;
+using RacingProject.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RacingProject.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ChartsController : ControllerBase
+    {
+        private readonly RacingProjectContext _db;
+
+        public ChartsController(RacingProjectContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ChartElement>> Teams()
+        {
+            var chartElements = _db.Drivers
+                                   .GroupBy(driver => driver.TeamId)
+                                   .Select(group => new
+                                   {
+                                       TeamId = group.Key,
+                                       NumOfDrivers = group.Count()
+                                   })
+                                   .Join(_db.Teams, x => x.TeamId, team => team.Id, (x, team) =>
+                                   new ChartElement(team.Name, x.NumOfDrivers)).ToList();
+
+            return chartElements;
+        }
+    }
+}
